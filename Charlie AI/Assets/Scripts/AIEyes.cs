199133f2297ea0ceec22eafd375950e2a84e8cd8
Charlie Mao <118494216+maocharliemao@users.noreply.github.com>
2024-02-08 12:48:02 +1100

    added turn towards

--- Charlie AI/Assets/Scripts/AIEyes.cs
+++ Charlie AI/Assets/Scripts/AIEyes.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class AIEyes : MonoBehaviour
 {
-    
+    public int rays = 10;
+    public float maxAngle = 1f;
+
+    private void FixedUpdate()
+    {
+        for (int i = -rays/2; i <= rays/2; i++)
+        {
+     
+            float spreadAngle = -maxAngle/(rays-1);
+            Vector3 dir = Quaternion.Euler(0, i*spreadAngle, 0) * transform.forward;
+
+
+            Debug.DrawRay(transform.position, dir * 20f, Color.yellow);
+        }
+    }
+
 }
--- Charlie AI/Assets/Scripts/TurnTowards.cs
+++ Charlie AI/Assets/Scripts/TurnTowards.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TurnTowards : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 targetPosition;
+    public float turnspeed = 2;
+    public Transform target;
+    public Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 targetDir;
+
+
+        if (target != null)
+        {
+            targetDir = target.position - transform.position;
+        }
+        else
+        {
+            targetDir = targetPosition - transform.position;
+        }
+
+
+        float angle = Vector3.SignedAngle(transform.forward, targetDir, Vector3.up);
+
+        Vector3 torque = Vector3.up * angle * turnspeed;
+
+
+        rb.AddRelativeTorque(torque);
     }
 }

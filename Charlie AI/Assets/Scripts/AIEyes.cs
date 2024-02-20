2d1fc6780366b4e0dba9711ac0f5879a251460bc
Charlie Mao <118494216+maocharliemao@users.noreply.github.com>
2024-02-20 16:28:19 +1100

    Eyes for boids

--- Charlie AI/Assets/Scripts/AIEyes.cs
+++ Charlie AI/Assets/Scripts/AIEyes.cs
@@ -6,17 +6,37 @@ public class AIEyes : MonoBehaviour
 {
     public int rays = 10;
     public float maxAngle = 1f;
+    public float detectionRange = 20f;
+    public LayerMask detectionLayer;
+    public Avoid avoidScript;
+
+    public List<Transform> detectedObjects = new List<Transform>();
+
+    private void Start()
+    {
+        avoidScript = GetComponent<Avoid>();
+    }
 
     private void FixedUpdate()
     {
-        for (int i = -rays/2; i <= rays/2; i++)
+        detectedObjects.Clear();
+        for (int i = -rays / 2; i <= rays / 2; i++)
         {
-     
-            float spreadAngle = -maxAngle/(rays-1);
-            Vector3 dir = Quaternion.Euler(0, i*spreadAngle, 0) * transform.forward;
-            Debug.DrawRay(transform.position, dir * 20f, Color.yellow);
+            float spreadAngle = -maxAngle / (rays - 1);
             
+            Vector3 dir = Quaternion.Euler(0, i * spreadAngle, 0) * transform.forward;
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, dir, out hit, detectionRange, detectionLayer))
+            {
+                detectedObjects.Add(hit.transform);
+                avoidScript.AvoidObstacle();
+                Debug.DrawRay(transform.position, dir * hit.distance, Color.red);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, dir * detectionRange, Color.yellow);
+            }
         }
     }
-
 }
--- Charlie AI/Assets/Scripts/Avoid.cs
+++ Charlie AI/Assets/Scripts/Avoid.cs
@@ -5,12 +5,8 @@ public class Avoid : MonoBehaviour
     public Rigidbody rb;
     public LayerMask layerMask;
     public float distance = 10f;
-    
     public float turnSpeed = 5f;
 
-
-
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +14,17 @@ public class Avoid : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        if (Physics.Raycast(transform.localPosition, transform.forward, distance, layerMask))
+      
+    }
+
+
+    public void AvoidObstacle()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, distance, layerMask))
         {
             rb.AddRelativeTorque(0, turnSpeed, 0);
-           
         }
     }
 }
